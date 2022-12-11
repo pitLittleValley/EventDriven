0c94142f82d8906c52e769981a2b25823cba44ad
PJ Cabezas Vallejo <pedroj.cabezasvallejo@gmail.com>
2022-12-11 19:27:17 +0100

    Review annotation

--- OrdersService/src/main/java/com/eventdriven/axon/demo/ordersservice/event/OrderCreatedEvent.java
+++ OrdersService/src/main/java/com/eventdriven/axon/demo/ordersservice/event/OrderCreatedEvent.java
@@ -1,9 +1,13 @@
 package com.eventdriven.axon.demo.ordersservice.event;
 
 import com.eventdriven.axon.demo.ordersservice.command.OrderStatus;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderCreatedEvent {
 
     private String orderId;
